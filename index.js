0701347b727e1bdcb0e105d0b3d832c6072dd040
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-12 16:46:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
@@ -23,6 +23,7 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
+// Login
 app.post("/api/login", async (req, res) => {
   const { email, password } = req.body;
 
@@ -42,6 +43,7 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
+// Sign up
 app.post("/api/signup", async (req, res) => {
   const { username, email, password, securityQ, securityQAnswer } = req.body;
 
@@ -50,8 +52,14 @@ app.post("/api/signup", async (req, res) => {
       return res.status(401).json({ msg: "User already exists" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
-    const hashedAnswer = await bcrypt.hash(securityQAnswer, 10)
-    const newUser = new User({ username, email, password: hashedPassword, securityQ, securityQAnswer: hashedAnswer  });
+    const hashedAnswer = await bcrypt.hash(securityQAnswer, 10);
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+      securityQ,
+      securityQAnswer: hashedAnswer,
+    });
     await newUser.save();
 
     res.json({ msg: "User registered successfully" });
@@ -61,6 +69,56 @@ app.post("/api/signup", async (req, res) => {
   }
 });
 
+// forgotPassword
+app.post("/api/verifyEmail", async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: "No account found !" });
+    }
+    return res.status(200).json({ question: user.securityQ });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+app.post("/api/verifyAnswer", async (req, res) => {
+  const { email, answer } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ msg: "No account found !" });
+    }
+    if (await bcrypt.compare(answer, user.securityQAnswer)) {
+      return res.status(200).json({ msg: "Answer correct !" });
+    } else {
+      return res.status(400).json({ msg: "Incorrect Answer" });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+app.post("/api/newPassword", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).json({ msg: "User not found" });
+
+    user.password = await bcrypt.hash(password, 10);
+    await user.save();
+
+    res.status(200).json({ msg: "Password updated successfully" });
+  } catch (error) {
+    res.status(500).json({ msg: "Server error" });
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
