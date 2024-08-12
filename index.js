ea39509d410b2284a875e610224b61519fa15b48
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-12 18:14:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,13 +7,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
-app.use(cors(corsOptions)) 
+app.use(cors()) 
 app.use(express.json());
 const port = 5000;
 
