62e0b955ade1dd4de11a358bb22e5abb0f2cd77a
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-12 17:51:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+const corsOptions = {
+  origin: 'https://upfront.onrender.com',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 const port = 5000;
 
