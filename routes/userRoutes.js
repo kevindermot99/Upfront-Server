9124747d41726381379c3d50c86092091d3a8728
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-13 10:42:55 +0200

    Create userRoutes.js

---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+// routes/userRoutes.js
+const express = require('express');
+const { getAllUsers } = require('../controllers/userController');
+const router = express.Router();
+
+router.get('/', getAllUsers);
+
+module.exports = router;
