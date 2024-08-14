5970348cb2dfdcde99dc8bbc5cef7755ef885a9c
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-14 12:03:46 +0200

    Create user.js

---
+++ models/user.js
@@ -0,0 +1,17 @@
+
+const mongoose = require('mongoose');
+
+// Define the user schema
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  securityQ: { type: String, required: true },
+  securityQAnswer: { type: String, required: true },
+});
+
+// Create the model
+const User = mongoose.model('User', userSchema);
+
+// Export the model
+module.exports = User;
