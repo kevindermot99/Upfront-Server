0c8fa8defb12df0313a80b2203ce785bfb7e3fed
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-16 11:36:54 +0200

    Update workspaces.js

--- models/workspaces.js
+++ models/workspaces.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 // Define the user schema
 const workspaceSchema = new mongoose.Schema({
   workspace1: { type: String, required: true },
-  workspace2: { type: String, required: true },
-  workspace3: { type: String, required: true },
   user_email: { type: String, required: true },
 });
 
