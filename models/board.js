20382583ac6cfcb5dfe7bdc9e0f8747f582ef007
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-20 10:07:27 +0200

    Create board.js

---
+++ models/board.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+
+// Define the user schema
+const boardSchema = new mongoose.Schema({
+  name: { type: String, required: false },
+  projectId: { type: String, required: false },
+  user_email: { type: String, required: false },
+}, { timestamps: { createdAt: true, updatedAt: true } });
+
+// Create the model
+const Board = mongoose.model("board", boardSchema);
+
+// Export the model
+module.exports = Board;
