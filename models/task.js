50d934f003625a691a2808bc1d4f967277806727
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-21 07:30:22 +0200

    Create task.js

---
+++ models/task.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+
+// Define the user schema
+const taskSchema = new mongoose.Schema({
+  name: { type: String, required: false },
+  startingOn: { type: String, required: false },
+  due: { type: String, required: false },
+  priority: { type: String, required: false },
+  boardId: { type: String, required: false },
+  curentStatus: { type: String, required: false },
+  projectId: { type: String, required: false },
+  userEmail: { type: String, required: false, match: /.+\@.+\..+/  },
+  assignedTo: [{ type: String, required: false, match: /.+\@.+\..+/ }]
+}, { timestamps: { createdAt: true, updatedAt: true } });
+
+// Create the model
+const Task = mongoose.model("task", taskSchema);
+
+// Export the model
+module.exports = Task;
