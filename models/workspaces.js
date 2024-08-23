40f32aa9c7dba80876a53024a3883b1dd6b874d5
kevindermot99 <mbonimpayekevin@gmail.com>
2024-08-23 22:05:20 +0200

    Update workspaces.js

--- models/workspaces.js
+++ models/workspaces.js
@@ -1,14 +1,10 @@
-
 const mongoose = require('mongoose');
-
 // Define the user schema
 const workspaceSchema = new mongoose.Schema({
   workspace1: { type: String, required: false },
   user_email: { type: String, required: true },
 });
-
 // Create the model
 const Workspace = mongoose.model('Workspace', workspaceSchema);
-
 // Export the model
 module.exports = Workspace;
